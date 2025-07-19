add5f6487a23aa0b69fd2371b16ba481a91844b7
Shruti Mohanty <shrutimohanty021@gmail.com>
2025-07-19 20:18:08 +0545

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/16mMhz0G1LUxcIGp8rL_LPywPOWzg4K6A/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1or7Q5B2QQPEl_7KBxiG4_Y41zY90ZK65/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
